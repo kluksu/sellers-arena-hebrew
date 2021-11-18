d9448a70d2136ecbca9eb1dd8e323eab8879d530
kluksu <shriki618@gmail.com>
2021-11-18 13:33:57 +0200

    push

--- src/components/utils.js
+++ src/components/utils.js
@@ -2,9 +2,9 @@ import axios from "axios";
 import { v4 as uuidv4 } from "uuid";
 
 // Example POST method implementation:
-//http://supplierzz.herokuapp.com
+//https://supplierzz.herokuapp.com
 //https://supplierzz.westeurope.cloudapp.azure.com
-export let domain = "http://supplierzz.herokuapp.com";
+export let domain = "https://supplierzz.herokuapp.com";
 export async function postData(URL = "", data = {}, token) {
   // Default options are marked with *
   const response = await fetch(URL, {
