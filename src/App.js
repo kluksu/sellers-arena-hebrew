33314ab8e64a14308d3e3f7a9860572483ccb887
kluksu <shriki618@gmail.com>
2021-12-17 21:36:25 +0200

    allorders dates gtg, needs account filter

--- src/App.js
+++ src/App.js
@@ -1056,6 +1056,7 @@ class App extends React.Component {
               sellerApprovedOrders={this.state.sellerApprovedOrders}
               MySupplierOrders={this.state.MySupplierOrders}
               activeAccount={this.state.activeAccount}
+              accessToken={this.state.accessToken}
             ></AllOrders>
           </Route>
 
--- src/pages/AllOrders.js
+++ src/pages/AllOrders.js
@@ -1,47 +1,109 @@
+import axios from "axios";
 import React, { Component } from "react";
-import { Col, Form, Row } from "react-bootstrap";
+import { Col, Form, Row, Table } from "react-bootstrap";
 import MyDateRange from "../components/MyDateRange";
+import { domain } from "../components/utils";
 
 class AllOrders extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedOrdersStatus: "all",
+      selectedOrdersStatus: "",
       MySupplierOrders: [],
       sellerApprovedOrders: [],
       fulfilledOrders: [],
       payedOrders: [],
       selectedUser: "",
       activeOrders: [],
+      startDate: new Date(),
+      endDate: new Date(),
     };
   }
+  handleSelect = (ranges) => {
+    console.log(ranges.selection.startDate, ranges.selection.endDate);
+    this.setState({
+      startDate: ranges.selection.startDate,
+      endDate: ranges.selection.endDate,
+    });
+  };
+  getFilteredOrders = (status, startDate, endDate, otherAccount) => {
+    const authorization = !this.props.accessToken
+      ? null
+      : `Bearer ${this.props.accessToken}`;
+    const config = {
+      headers: { "Content-Type": "application/json", authorization },
+    };
+    // status = `order_status=${status}`;
+    // startDate = `&submitted_at__gte${startDate}`;
+    // endDate = `&submitted_at__lte${endDate}`;
+    // otherAccount =
+    //   this.props.activeAccount && this.props.activeAccount.account_type == 3
+    //     ? `&buyer_account${otherAccount}`
+    //     : `&seller_account${otherAccount}`;
+
+    let path =
+      this.props.activeAccount.account_type == 2
+        ? "my-account-orders"
+        : "supplier-orders";
+    endDate = `${JSON.stringify(endDate).substring(1, 11)}T00%3a00%3a00`;
+    startDate = `${JSON.stringify(startDate).substring(1, 11)}T00%3a00%3a00`;
+    console.log(startDate);
+    axios
+      .get(
+        `${domain}/${path}/?&submitted_at__gte=${startDate}&submitted_at__lte=${endDate}&order_status=${status}`,
+        config
+      )
+      .then((data) => {
+        if (data.status == 200) {
+          console.log(data);
+          this.setState({ activeOrders: data.data.results });
+          // console.log(data.data.results);
+          // this.setState({ [state]: data.data.results });
+        }
+      });
+  };
   componentDidUpdate(prevProps, prevState) {
-    if (this.state.selectedOrdersStatus !== prevState.selectedOrdersStatus) {
-      console.log(this.state.selectedOrdersStatus);
-      this.chooseOrdersStatus();
+    if (
+      this.state.endDate !== prevState.endDate ||
+      this.state.startDate !== prevState.startDate ||
+      this.state.selectedOrdersStatus !== prevState.selectedOrdersStatus
+    ) {
+      console.log(this.state.startDate);
+      this.getFilteredOrders(
+        this.state.selectedOrdersStatus,
+        this.state.startDate,
+        this.state.endDate,
+        ""
+      );
     }
+    // if (this.state.selectedOrdersStatus !== prevState.selectedOrdersStatus) {
+    //   console.log(this.state.selectedOrdersStatus);
+    //   this.chooseOrdersStatus();
+    // }
   }
   handleChange = (event) => {
     this.setState({ [event.target.name]: event.target.value });
   };
-  chooseOrdersStatus = () => {
-    if (this.state.selectedOrdersStatus === "all") {
-      this.setState({
-        activeOrders: this.props.MySupplierOrders.concat(
-          this.props.fulfilledOrders.concat(this.props.sellerApprovedOrders)
-        ),
-      });
-    } else {
-      this.setState({
-        activeOrders: this.props[this.state.selectedOrdersStatus],
-      });
-    }
-  };
+  // chooseOrdersStatus = () => {
+  //   if (this.state.selectedOrdersStatus === "all") {
+  //     this.setState({
+  //       activeOrders: this.props.MySupplierOrders.concat(
+  //         this.props.fulfilledOrders.concat(this.props.sellerApprovedOrders)
+  //       ),
+  //     });
+  //   } else {
+  //     this.setState({
+  //       activeOrders: this.props[this.state.selectedOrdersStatus],
+  //     });
+  //   }
+  // };
   onMount = () => {};
   render() {
     let orders = this.state.activeOrders.map((order) => {
+      console.log(order);
+
       return (
-        <div
+        <tr
           onClick={() => {
             window.location.assign(
               this.props.activeAccount.account_type == 3
@@ -50,8 +112,20 @@ class AllOrders extends Component {
             );
           }}
         >
-          {order.id}
-        </div>
+          <td>{order.id}</td>
+          <td>
+            {" "}
+            {`${order.submitted_at.replace("T", " ").slice(0, 19)}    `} {}
+          </td>
+          <td>{order.order_status}</td>
+          <td>{order.user}</td>
+          <td>
+            {this.props.activeAccount &&
+            this.props.activeAccount.account_type == 3
+              ? order.buyer_account.name
+              : order.seller_account.name}
+          </td>
+        </tr>
       );
     });
     return (
@@ -63,7 +137,11 @@ class AllOrders extends Component {
               MySupplierOrders={this.state.MySupplierOrders} */}
           <Col>
             {" "}
-            <MyDateRange></MyDateRange>
+            <MyDateRange
+              handleSelect={this.handleSelect}
+              startDate={this.state.startDate}
+              endDate={this.state.endDate}
+            ></MyDateRange>
           </Col>
           <Col>
             <Form.Group controlId="exampleForm.ControlSelect1">
@@ -73,17 +151,32 @@ class AllOrders extends Component {
                 name="selectedOrdersStatus"
                 as="select"
               >
-                <option value={"all"}>כל ההזמנות </option>
+                <option value={""}>כל ההזמנות </option>
 
-                <option value={"MySupplierOrders"}>הזמנות מחכות לאישור</option>
-                <option value={"sellerApprovedOrders"}>
-                  הזמנות לפני משלוח
-                </option>
-                <option value={"fulfilledOrders"}>הזמנות שנשלחו</option>
-                <option value={"payedOrders"}>הזמנות ששולמו</option>
+                <option value={"submitted"}>הזמנות מחכות לאישור</option>
+                <option value={"seller_approved"}>הזמנות לפני משלוח</option>
+                <option value={"filled"}>הזמנות שנשלחו</option>
+                <option value={"payed"}>הזמנות ששולמו</option>
               </Form.Control>
             </Form.Group>
-            {orders}
+            <Table hover className="transactionsList">
+              <thead>
+                <tr>
+                  <th>מספר הזמנה</th>
+                  <th>תאריך</th>
+                  <th>סטאטוס</th>
+                  <th>נוצרה ע"י</th>
+                  <th>
+                    {" "}
+                    {this.props.activeAccount &&
+                    this.props.activeAccount.account_type == 3
+                      ? "שם קונה"
+                      : "שם מוכר"}
+                  </th>
+                </tr>
+              </thead>
+              <tbody>{orders}</tbody>
+            </Table>
           </Col>
           <Col>
             <Form.Group controlId="exampleForm.ControlSelect1">
